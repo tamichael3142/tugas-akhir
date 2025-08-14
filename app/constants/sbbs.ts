43d9521a3ba16d2180ee637b7e5007576a658915
arielondeltahq <tamichael3142@gmail.com>
2025-08-14 21:45:29 +0700

    added some assets and data

--- app/constants/sbbs.ts
+++ app/constants/sbbs.ts
@@ -1,9 +1,21 @@
 const sbbs = {
   name: {
-    long: 'Sekolah Bunga Bangsa Surabaya',
     short: 'SBBS',
+    long: 'Sekolah Bunga Bangsa Surabaya',
   },
   siteDescription: 'Situs manajemen pembelajaran Sekolah Bunga Bangsa Surabaya.',
+  address: {
+    short: 'Jl Marina Emas Timur 1 No. 1',
+    long: 'Jl Marina Emas Timur 1 No. 1 Kec. Sukolilo, Surabaya 60111, Jawa Timur',
+  },
+  phone: '(031) 595-6282',
+  whatsapp: '082330035662 (WA Only)',
+  officialWebsite: 'https://bungabangsaschool.sch.id/',
+  map: {
+    link: 'https://maps.app.goo.gl/jHbMWBgwfA7mvZvS8',
+    embedSrc:
+      'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d506566.0280821895!2d112.805873!3d-7.292257000000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fa773fefd98b%3A0x549f60753bb720a3!2sBunga%20Bangsa%20Junior%20High%20School!5e0!3m2!1sen!2sus!4v1755182434504!5m2!1sen!2sus',
+  },
 }
 
 export default sbbs
