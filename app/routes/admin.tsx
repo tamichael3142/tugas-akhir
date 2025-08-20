e11d1cf509dea76a6643bc149f9ef7883b90700f
arielondeltahq <tamichael3142@gmail.com>
2025-08-20 21:12:28 +0700

    fix: test hilangkan a kecil

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,11 +1,11 @@
 import { MetaFunction } from '@remix-run/react'
 import constants from '~/constants'
-import AdminDashboardLayout from '~/layouts/admin/AdminDashboard'
+// import AdminDashboardLayout from '~/layouts/admin/AdminDashboard'
 
 export const meta: MetaFunction = () => {
   return constants.pageMetas.adminDefault
 }
 
 export default function AdminRoute() {
-  return <AdminDashboardLayout />
+  return <div />
 }
