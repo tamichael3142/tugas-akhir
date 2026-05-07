8c61b3dd9a52aa2b8fa02a2bb0bfd3fccd15acbe
Michael <tamichael3142@gmail.com>
2026-05-08 01:06:30 +0700

    Fix vercel 2
    - ganti package untuk sanitize html

--- app/utils/html.utils.ts
+++ app/utils/html.utils.ts
@@ -1,8 +1,15 @@
-import { sanitize } from 'isomorphic-dompurify'
+import sanitizeHtmlPackage from 'sanitize-html'
 
 function sanitizeHtml(html: string) {
-  return sanitize(html, {
-    USE_PROFILES: { html: true },
+  return sanitizeHtmlPackage(html, {
+    allowedTags: [...sanitizeHtmlPackage.defaults.allowedTags, 'img', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+    allowedAttributes: {
+      ...sanitizeHtmlPackage.defaults.allowedAttributes,
+      '*': ['class', 'style'],
+      a: ['href', 'name', 'target', 'rel'],
+      img: ['src', 'srcset', 'alt', 'title', 'width', 'height'],
+    },
+    allowedSchemes: ['http', 'https', 'data', 'mailto'],
   })
 }
 
